08f3994da0a2d8dd3956c7bb74c68876cddd71c0
sunitha1008 <sunitha091008@gmail.com>
2023-11-24 13:13:42 +0000

    ecomm public subnets created

--- vpc.tf
+++ vpc.tf
@@ -7,4 +7,22 @@ resource "aws_vpc" "tf-ecomm" {
     Name = "ecomm-vpc"
   }
 }
+# creating public subnets for ecomm-vpc in different availibility zones
+resource "aws_subnet" "ecomm-pub-sn-A" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "ecomm-pubsn-A"
+  }
+}
+
+resource "aws_subnet" "ecomm-pub-sn-B" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "ecomm-pubsn-B"
+  }
+}
 
